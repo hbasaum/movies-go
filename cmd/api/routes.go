af28430b58e32eed28b8dce30d8d5aef68a68dcb
hbasaum <hbasaum@gmail.com>
2025-09-12 19:07:51 +0600

    start again

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -9,9 +9,12 @@ import (
 func (app application) routes() http.Handler {
 	router := httprouter.New()
 
-	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
-	router.HandlerFunc(http.MethodGet, "/v1/movies", app.createMovieHandler)
-	router.HandlerFunc(http.MethodPost, "/v1/movies/:id", app.showMovieHandler)
+	router.NotFound = http.HandlerFunc(app.notFoundResponse)
+	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
 
-	return router
+	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
+
+	return app.recoverPanic(router)
 }
