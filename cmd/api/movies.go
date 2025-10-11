741f888ed33947bd942d03ab34337b52abf34d0e
hbasaum <hbasaum@gmail.com>
2025-10-11 09:08:21 +0600

    get and add

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
-	"time"
 
 	"github.com/hbasaum/lets-go-further/internal/data"
 	"github.com/hbasaum/lets-go-further/internal/validator"
@@ -63,13 +63,14 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 		app.badRequestResponse(w, r, err)
 	}
 
-	movie := data.Movie{
-		ID:        id,
-		CreatedAt: time.Now(),
-		Title:     "Casablanca",
-		Runtime:   102,
-		Genres:    []string{"drama", "romance", "war"},
-		Version:   1,
+	movie, err := app.models.Movies.Get(id)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
 	}
 
 	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
