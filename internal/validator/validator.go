1b290d571ce28b01b056d0e324148d9d395ed109
hbasaum <hbasaum@gmail.com>
2025-11-01 07:23:41 +0600

    user register

--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -39,6 +39,10 @@ func PermittedValue[T comparable](value T, permittedValues ...T) bool {
 	return slices.Contains(permittedValues, value)
 }
 
+func Matches(value string, rx *regexp.Regexp) bool {
+	return rx.MatchString(value)
+}
+
 func Unique[T comparable](values []T) bool {
 	uniqueValues := make(map[T]bool)
 
